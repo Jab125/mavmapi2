2cf5fd401bd3f43b3080fee71c0e4eae65513a20
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-12-23 23:21:08 +1100

    add item tooltip

---
+++ src/main/java/io/github/akashiikun/mavapi/impl/mixin/ItemStackMixin.java
@@ -0,0 +1,63 @@
+package io.github.akashiikun.mavapi.impl.mixin;
+
+import io.github.akashiikun.mavapi.api.v2.AxolotlVariant;
+import io.github.akashiikun.mavapi.api.v2.MavApiDataComponents;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentHolder;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.component.PatchedDataComponentMap;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.animal.axolotl.Axolotl;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+import net.minecraft.world.item.component.TooltipProvider;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Consumer;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin implements DataComponentHolder {
+	@Shadow
+	public abstract <T extends TooltipProvider> void addToTooltip(DataComponentType<T> component, Item.TooltipContext context, TooltipDisplay tooltipDisplay, Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag);
+
+	@Shadow
+	@Final
+	private PatchedDataComponentMap components;
+
+	@Inject(method = "addDetailsToTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;appendHoverText(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/Item$TooltipContext;Lnet/minecraft/world/item/component/TooltipDisplay;Ljava/util/function/Consumer;Lnet/minecraft/world/item/TooltipFlag;)V", shift = At.Shift.AFTER))
+	void mavapi$addDetailsToTooltip(Item.TooltipContext context, TooltipDisplay tooltipDisplay, @Nullable Player player, TooltipFlag tooltipFlag, Consumer<Component> tooltipAdder, CallbackInfo ci) {
+		this.mavm$addToTooltip(MavApiDataComponents.AXOLOTL_VARIANT, context, tooltipDisplay, tooltipAdder, tooltipFlag);
+		this.mavm$addToTooltip2(DataComponents.AXOLOTL_VARIANT, context, tooltipDisplay, tooltipAdder, tooltipFlag);
+	}
+
+	@Unique
+	private void mavm$addToTooltip(
+			DataComponentType<Holder<AxolotlVariant>> component, Item.TooltipContext context, TooltipDisplay tooltipDisplay, Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag
+	) {
+		Holder<AxolotlVariant> tooltipProvider = this.get(component);
+		if (tooltipProvider != null && tooltipDisplay.shows(component)) {
+			new AxolotlVariant.VariantTooltipProvider(tooltipProvider).addToTooltip(context, tooltipAdder, tooltipFlag, this.components);
+		}
+	}
+
+	@Unique
+	private void mavm$addToTooltip2(
+			DataComponentType<Axolotl.Variant> component, Item.TooltipContext context, TooltipDisplay tooltipDisplay, Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag
+	) {
+		Axolotl.Variant tooltipProvider = this.get(component);
+		if (tooltipProvider != null && tooltipDisplay.shows(component)) {
+			new AxolotlVariant.VariantTooltipProvider(context.registries(), tooltipProvider).addToTooltip(context, tooltipAdder, tooltipFlag, this.components);
+		}
+	}
+}
