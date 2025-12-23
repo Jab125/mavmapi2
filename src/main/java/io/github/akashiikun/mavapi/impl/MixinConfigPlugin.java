0d74058995bda808690c049fab463676f2334fca
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-12-24 01:10:53 +1100

    it now works in non-mojmap environments

--- src/main/java/io/github/akashiikun/mavapi/impl/MixinConfigPlugin.java
+++ src/main/java/io/github/akashiikun/mavapi/impl/MixinConfigPlugin.java
@@ -2,8 +2,14 @@ package io.github.akashiikun.mavapi.impl;
 
 import com.google.gson.Gson;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
@@ -11,6 +17,7 @@ import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 import org.objectweb.asm.util.Printer;
 import org.objectweb.asm.util.Textifier;
 import org.objectweb.asm.util.TraceMethodVisitor;
@@ -18,15 +25,56 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.IntStream;
 
 public class MixinConfigPlugin implements IMixinConfigPlugin {
+	private static final Map<String, String> nameMap = new HashMap<>();
 	@Override
 	public void onLoad(String mixinPackage) {
+		byte[] bytes = null;
+		try {
+			bytes = MixinConfigPlugin.class.getResourceAsStream("/io/github/akashiikun/mavapi/impl/asm/_Dummy.class").readAllBytes();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		ClassReader reader = new ClassReader(bytes);
+		ClassVisitor visitor = new ClassVisitor(Opcodes.ASM9) {
+			@Override
+			public MethodVisitor visitMethod(int access, String methodName, String methodDescriptor, String signature, String[] exceptions) {
+				return new MethodVisitor(Opcodes.ASM9) {
+					@Override
+					public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
+						if (false) {
+							System.out.println(descriptor + ", " + visible);
+						}
+						return new AnnotationVisitor(Opcodes.ASM9) {
+							@Override
+							public void visit(String name, Object value) {
+								if ("Lio/github/akashiikun/mavapi/impl/asm/_Dummy$Name;".equals(descriptor) && "value".equals(name) && value instanceof String string) {
+									if ("Axolotl$Variant".equals(string) || "RandomSource".equals(string)) {
+										nameMap.put(string, Type.getReturnType(methodDescriptor).getInternalName());
+									} else {
+										nameMap.put(string, methodName);
+									}
+								}
+								super.visit(name, value);
+							}
+						};
+					}
+				};
+			}
+		};
+		reader.accept(visitor, 0);
 
+		if (false) {
+			System.out.println(nameMap);
+		}
 	}
 
 	@Override
@@ -50,12 +98,31 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
 	}
 
 
+	public static String Axolotl$Variant() {
+		return nameMap.get("Axolotl$Variant");
+	}
+
+	public static String readAdditionalSaveData() {
+		return nameMap.get("readAdditionalSaveData");
+	}
+
+	public static String getBreedOffspring() {
+		return nameMap.get("getBreedOffspring");
+	}
+	public static String setPersistenceRequired() {
+		return nameMap.get("setPersistenceRequired");
+	}
+
+	public static String RandomSource() {
+		return nameMap.get("RandomSource");
+	}
+
 	// TODO mappings
 	@Override
 	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
 		if (mixinClassName.contains("AxolotlMixin") && !mixinClassName.contains("$")) {
 			{
-				MethodNode readAdditionalSaveData = targetClass.methods.stream().filter(a -> a.name.equals("readAdditionalSaveData")).findFirst().orElseThrow();
+				MethodNode readAdditionalSaveData = targetClass.methods.stream().filter(a -> a.name.equals(readAdditionalSaveData())).findFirst().orElseThrow();
 				InsnList oldList = readAdditionalSaveData.instructions;
 
 
@@ -68,12 +135,20 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
 				}
 				AbstractInsnNode from = IntStream.iterate(readAdditionalSaveData.instructions.indexOf(node), i -> i > 0, i -> i - 1).filter(i -> readAdditionalSaveData.instructions.get(i) instanceof LabelNode).mapToObj(i -> (LabelNode) readAdditionalSaveData.instructions.get(i)).findFirst().orElse(null);//readAdditionalSaveData.instructions.get(readAdditionalSaveData.instructions.indexOf(node) - 1);
 				AbstractInsnNode to = null;
+				boolean getTheNextOne = false;
 				for (AbstractInsnNode instruction : readAdditionalSaveData.instructions) {
-					if (instruction instanceof MethodInsnNode insnNode && insnNode.getOpcode() == Opcodes.INVOKEVIRTUAL && ((MethodInsnNode) instruction).name.equals("setVariant")) {
-						System.out.println(insnNode.name + " " + insnNode.owner + " " + insnNode.desc);
-						to = insnNode;
+					if (getTheNextOne) {
+						to = instruction;
 						break;
 					}
+					if (instruction instanceof TypeInsnNode insnNode && insnNode.getOpcode() == Opcodes.CHECKCAST && insnNode.desc.equals(Axolotl$Variant())) {
+						getTheNextOne = true;
+					}
+//					if (instruction instanceof MethodInsnNode insnNode && insnNode.getOpcode() == Opcodes.INVOKEVIRTUAL && ((MethodInsnNode) instruction).name.equals("setVariant")) {
+//						System.out.println(insnNode.name + " " + insnNode.owner + " " + insnNode.desc);
+//						to = insnNode;
+//						break;
+//					}
 				}
 				InsnList list = new InsnList();
 
@@ -89,28 +164,40 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
 					if (abstractInsnNode == to) adding = true;
 				}
 				readAdditionalSaveData.instructions = list;
-				System.out.println("Printing: " + printer.text);
-				printer.print(new PrintWriter(System.out));
+				if (false) {
+					System.out.println("Printing: " + printer.text);
+					printer.print(new PrintWriter(System.out));
+				}
 			}
 
 			{
-				MethodNode getBreedOffspring = targetClass.methods.stream().filter(a -> a.name.equals("getBreedOffspring")).findFirst().orElseThrow();
+				MethodNode getBreedOffspring = targetClass.methods.stream().filter(a -> a.name.equals(getBreedOffspring())).findFirst().orElseThrow();
 				InsnList oldList = getBreedOffspring.instructions;
 
 
 				FieldInsnNode node = null;
 				for (AbstractInsnNode instruction : getBreedOffspring.instructions) {
-					if (instruction instanceof FieldInsnNode insnNode && insnNode.desc.contains("RandomSource")) {
+					if (instruction instanceof FieldInsnNode insnNode && insnNode.desc.contains(RandomSource())) {
 						node = insnNode;
 						break;
 					}
 				}
 				AbstractInsnNode from = IntStream.iterate(getBreedOffspring.instructions.indexOf(node), i -> i > 0, i -> i - 1).filter(i -> getBreedOffspring.instructions.get(i) instanceof LabelNode).mapToObj(i -> (LabelNode) getBreedOffspring.instructions.get(i)).findFirst().orElse(null);//readAdditionalSaveData.instructions.get(readAdditionalSaveData.instructions.indexOf(node) - 1);
 				AbstractInsnNode to = null;
+				AbstractInsnNode upTo = null;
 				for (AbstractInsnNode instruction : getBreedOffspring.instructions) {
-					if (instruction instanceof MethodInsnNode insnNode && insnNode.getOpcode() == Opcodes.INVOKEVIRTUAL && ((MethodInsnNode) instruction).name.equals("setVariant")) {
-						System.out.println(insnNode.name + " " + insnNode.owner + " " + insnNode.desc);
-						to = insnNode;
+					if (instruction instanceof MethodInsnNode insnNode && insnNode.getOpcode() == Opcodes.INVOKEVIRTUAL && ((MethodInsnNode) instruction).name.equals(setPersistenceRequired())) {
+						if (false) {
+							System.out.println(insnNode.name + " " + insnNode.owner + " " + insnNode.desc);
+						}
+						upTo = instruction;
+						break;
+					}
+				}
+				for (int i = getBreedOffspring.instructions.indexOf(upTo) - 1; i >= 0; i--) {
+					AbstractInsnNode abstractInsnNode = getBreedOffspring.instructions.get(i);
+					if (abstractInsnNode instanceof MethodInsnNode insnNode && insnNode.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+						to = abstractInsnNode;
 						break;
 					}
 				}
@@ -128,8 +215,10 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
 					if (abstractInsnNode == to) adding = true;
 				}
 				getBreedOffspring.instructions = list;
-				System.out.println("Printing: " + printer.text);
-				printer.print(new PrintWriter(System.out));
+				if (false) {
+					System.out.println("Printing: " + printer.text);
+					printer.print(new PrintWriter(System.out));
+				}
 			}
 
 //			while (true) {
